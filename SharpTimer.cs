0a7656675777364a2a76601b83ec39a113076a83
DEAFPS <grcjn.brcki@gmail.com>
2023-12-16 06:27:37 +0100

    fix typo

--- SharpTimer.cs
+++ SharpTimer.cs
@@ -407,7 +407,7 @@ namespace SharpTimer
                 playerTimers[player.Slot].StartZoneC2 = "";
                 playerTimers[player.Slot].IsAddingStartZone = true;
                 playerTimers[player.Slot].IsAddingEndZone = false;
-                player.PrintToChat($" {ChatColors.LightPurple}[ZONE TOOL]{ChatColors.Default} Please stand on one of opposite the start zone corners and type {ChatColors.Green}!c1 & !c2");
+                player.PrintToChat($" {ChatColors.LightPurple}[ZONE TOOL]{ChatColors.Default} Please stand on one of the opposite start zone corners and type {ChatColors.Green}!c1 & !c2");
                 player.PrintToCenter($" {ChatColors.Grey}Please stand on one of the opposite start zone corners and type !c1 & !c2");
                 player.PrintToChat($" {ChatColors.Grey}Type !addendzone again to cancel...");
                 player.PrintToChat($" {ChatColors.Grey}Commands:!addstartzone, !addendzone,");
@@ -436,7 +436,7 @@ namespace SharpTimer
                 playerTimers[player.Slot].EndZoneC2 = "";
                 playerTimers[player.Slot].IsAddingStartZone = false;
                 playerTimers[player.Slot].IsAddingEndZone = true;
-                player.PrintToChat($" {ChatColors.LightPurple}[ZONE TOOL]{ChatColors.Default} Please stand on one of opposite the end zone corners and type {ChatColors.Green}!c1 & !c2");
+                player.PrintToChat($" {ChatColors.LightPurple}[ZONE TOOL]{ChatColors.Default} Please stand on one of the opposite end zone corners and type {ChatColors.Green}!c1 & !c2");
                 player.PrintToCenter($" Please stand on one of the opposite end zone corners and type !c1 & !c2");
                 player.PrintToChat($" {ChatColors.Grey}Type !addendzone again to cancel...");
                 player.PrintToChat($" {ChatColors.Grey}Commands:!addstartzone, !addendzone,");
